5bc8e2948ef9f3a9b5354f87860e705587f46e6f
ming <zzzzz124987@gmail.com>
2024-08-27 11:52:09 +0800

    [Hank] Feat: 引用google font服務

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,16 @@ export default defineNuxtConfig({
   modules: [
     "@element-plus/nuxt", "@nuxtjs/google-fonts", "@pinia/nuxt"
   ],
+  googleFonts: {
+    families: {
+      'Noto Sans TC': true,
+      'Roboto': true,
+    },
+    download: true, // 將字體下載到本地，並打包到專案裡，防止用戶訪問不了google字體服務
+    base64: false, // 不要 base64格式，不然首頁下載文件會非常大
+    overwriting: true,
+    outputDir: 'assets/fonts' // 本地字體文件目錄，下載的字體會放到這個目錄裡
+  },
   css: [
     "element-plus/dist/index.css",
     '~/assets/sass/main.sass'
